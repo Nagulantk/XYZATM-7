aeb477e4afac2aa351153512be36713d070ac77e
Nagulantk <114421488+Nagulantk@users.noreply.github.com>
2022-11-08 11:06:10 +0530

    Delete Model directory

--- Model/DebitCard.java
+++
@@ -1,8 +0,0 @@
-package Model;
-import java.time.LocalDate;
-public class DebitCard extends Card {
-     public DebitCard(int cardNumber, int pin, int ccv,LocalDate expiryDate,String cardType)
-    {
-        super( cardNumber, pin, ccv,expiryDate,cardType);
-    }
-}
