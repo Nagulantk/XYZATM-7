6d0d268920e36fc49678ad438c3ce6d0502d744c
Nagulantk <114421488+Nagulantk@users.noreply.github.com>
2022-11-10 09:19:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import Controller.MainMenu;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args)  {
+        MainMenu main=new MainMenu();
+        main.mainWorking();
+    }
+}
